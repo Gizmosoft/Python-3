cb43dcfd5d77e29ccf9c2e281c5e1ba1e8d660f6
Kartikey V Hebbar <kartikey.hebbar@gmail.com>
2018-03-20 12:29:00 +0530

    Initial commit

---
+++ arbit.py
@@ -0,0 +1,2 @@
+import urllib.request
+urllib.request.urlretrieve("https://www.imagesbazaar.com/Previewdonework.aspx?id=773536&s=29&gid=16191&Keyid=technology%20&idtot=technology&exec=&nonexec=&ddlNoPeople=0&ddllocation=0&chkOrientation=&searchwithin=&view=0&Type=Key&PNO=51&mostview=0&pagingshow=0&totalimg=104543&pageno=0&mostpop=96&st=0","local.jpg")
